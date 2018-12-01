66b0ff8a58b1c18c2d798b5292c269cbf82e9ba8
Cyrus Vahidi <cyrusvahidi@gmail.com>
2018-12-01 23:58:41 +0000

    init commit new

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const port = process.env.PORT || 5000;
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io').listen(server);
+app.use(express.static(path.join(__dirname, '../src/public')));
+
+io.on('connection', function(client) {
+  console.log('a user connected');
+  console.log(client.id);
+
+  client.on('subscribe', (interval) => {
+    console.log('client is subscribing to timer with interval ', interval);
+    setInterval(() => {
+      client.emit('timer', new Date());
+    }, interval);
+  });
+
+  client.on("message-from-browser", function (obj) {
+   console.log(JSON.stringify(obj));
+   echoInput(`${obj.message} back at you from the server`);
+  });
+
+
+  client.on('disconnect', () => {
+    console.log('user disconnected')
+  });
+});
+
+server.listen(port);
+console.log(`Node server running on port ${port}...`);
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var webpack = require("webpack");
+
+var package = require('./package.json');
+var path = require("path");
+var BUILD_DIR = path.resolve(__dirname, 'src/public');
+var APP_DIR = path.resolve(__dirname, 'src/app');
+
+module.exports = {
+  context: __dirname + "/src",
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'js/bundle.js'
+  },
+  devServer: {
+    contentBase: 'src/public',
+    port: 3000,
+    historyApiFallback: true,
+    proxy: {
+      "/api/*":{
+          target:"http://localhost:5000/",
+          secure:"false"
+      }
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+
+    ]
+  }
+};
